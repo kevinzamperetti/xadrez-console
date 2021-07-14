a27ecc721ed8d44360dbad37cdb17f088adf8108
Kevin <kevin.zamperetti92@gmail.com>
2021-07-14 19:00:41 -0300

    Iniciando implementacção método parta imprimir tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,10 +7,10 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao p = new Posicao(3, 4);
             Tabuleiro tab = new Tabuleiro(8,8);
 
-            Console.WriteLine(p);
+            Tela.imprimirTabuleiro(tab);
+
         }
     }
 }
